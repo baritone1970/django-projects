26e964fed992508588cf7418140fe168a5a756c1
baritone1970 <baritone@inbox.ru>
2022-08-14 23:42:21 +0300

    Промежуточный нерабочий код.

--- D2_9/NewsPortal/views.py
+++ D2_9/NewsPortal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post    # Класс Post, который мы определили в файле models.py
@@ -11,14 +12,14 @@ def news(request):
 
 class NewsList(ListView):
    model = Post
-   ordering = 'time_of_creation'    # Поле, которое будет использоваться для сортировки объектов
+   ordering = '-time_of_creation'    # Поле для сортировки объектов, "-blabla" - обратный отсчёт
    template_name = 'news.html'
-    # Его надо указать, чтобы обратиться к списку объектов в
    context_object_name = 'news'   # Имя списка, через который обращаются ко всем объектам в html-шаблоне.
-   paginate_by = 10
+   paginate_by = 10 #TODO - разобраться с переходом к следующей странице при пажинации
 
    # наследуется метод .as_view()
 
+#TODO - ещё не понял, как разобраться с сортировкой только новостей!
    # Переопределяем функцию получения списка товаров
    def get_queryset(self):
        # Получаем обычный запрос
@@ -32,8 +33,15 @@ class NewsList(ListView):
        # Возвращаем из функции отфильтрованный список товаров
        return self.filterset.qs
 
+   # Метод get_context_data позволяет изменить набор данных, который будет передан в шаблон.
    def get_context_data(self, **kwargs):
+       # С помощью super() мы обращаемся к родительским классам
+       # и вызываем у них метод get_context_data с теми же аргументами,
+       # что и были переданы нам.
+       # В ответе мы должны получить словарь.
        context = super().get_context_data(**kwargs)
+       # К словарю добавим текущую дату в ключ 'time_now'.
+       context['time_now'] = datetime.utcnow()
        # Добавляем в контекст объект фильтрации.
        context['filterset'] = self.filterset
        return context
