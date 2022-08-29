7c14662d9378197188972dc2ce3c4091584bd314
baritone1970 <baritone@inbox.ru>
2022-08-29 22:44:45 +0300

    Соответствует уровню D4, с недостатками

--- D2_9/NewsPortal/filters.py
+++ D2_9/NewsPortal/filters.py
@@ -1,4 +1,5 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, DateTimeFilter
+from django.forms import DateTimeInput
 from .models import Post
 
 # Создаем свой набор фильтров выдачи данных для модели Post.
@@ -6,12 +7,20 @@ from .models import Post
 class NewsFilter(FilterSet):
     # класс FilterSet позволяет создавать фильтры через поля подкласса Meta
     # см. https://django-filter.readthedocs.io/en/stable/ref/filterset.html#automatic-filter-generation-with-model
-   class Meta:
-       model = Post # модель, в которой будем фильтровать записи.
-       # В fields указывается, по каким полям модели будут работать создаваемые через Meta фильтры
-       fields = {
-           #    поиск по названию; по категории; позже указываемой даты.
-           'header': ['contains'],
-           'category': ['contains'],
-           'time_of_creation': ['date__gt'],
-       }
+    added_after = DateTimeFilter(
+        field_name='time_of_creation',
+        lookup_expr='gt',
+        widget=DateTimeInput(
+            format='%Y-%m-%dT%H:%M',
+            attrs={'type': 'datetime-local'},
+        ),
+    )
+    class Meta:
+        model = Post # модель, в которой будем фильтровать записи.
+        # В fields указывается, по каким полям модели будут работать создаваемые через Meta фильтры
+        fields = {
+            #    поиск по названию; по категории; позже указываемой даты.
+            'header': ['contains'],
+            'category': ['contains'],
+            #'time_of_creation': ['date__gt'],
+        }
